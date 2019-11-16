ed177d3401bcdcb98ea72c8afdd38c78cce8c116
ghtea <47841931+ghtea@users.noreply.github.com>
2019-11-16 19:07:56 +0900

    1

--- my_heroes_js.js
+++ my_heroes_js.js
@@ -1,37 +1,25 @@
-var trace1 = {
-  x: [1, 2, 3, 4, 5],
-  y: [1, 6, 3, 6, 1],
-  mode: 'markers+text',
-  type: 'scatter',
-  name: 'Team A',
-  text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-  textposition: 'top center',
-  textfont: {
-    family:  'Raleway, sans-serif'
-  },
-  marker: { size: 12 }
-};
-
-
-var data = [ trace1];
-
-var layout = {
-  xaxis: {
-    range: [ 0.75, 5.25 ]
-  },
-  yaxis: {
-    range: [0, 8]
-  },
-  legend: {
-    y: 0.5,
-    yref: 'paper',
-    font: {
-      family: 'Arial, sans-serif',
-      size: 20,
-      color: 'grey',
+// make data
+var chart = c3.generate({
+    data: {
+        url: '/My_Heroes_csv.csv',
+        type: 'scatter'
     }
-  },
-  title:'Data Labels on the Plot'
-};
+});
 
-Plotly.newPlot('myDiv', data, layout, {showSendToCloud: true});
+// make chart from data
+var chart = c3.generate({
+    bindto: '#target', // specify the DOM element to which the chart will be bound
+    data: chart, // specify the data to be used in the chart
+    size: { // specify the chart size
+      height: 340,
+      width: 680
+    },
+    axis: { // specify the x and y axis labels
+      x: {
+        label: 'Like',
+      },
+      y: {
+        label: 'Skilled'
+      }
+    }
+  });
