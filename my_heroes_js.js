0002536216a5d933ccaa9324cd60576cb2ef3650
ghtea <47841931+ghtea@users.noreply.github.com>
2019-11-16 19:25:17 +0900

    1

--- my_heroes_js.js
+++ my_heroes_js.js
@@ -1,14 +1,9 @@
-// make data
+// make chart
 var chart = c3.generate({
-    data: {
+    data: { // specify the data to be used in the chart
         url: 'my_heroes_csv.csv',
         type: 'scatter'
-    }
-});
-
-// make chart from data
-var chart = c3.generate({
-    data: chart, // specify the data to be used in the chart
+    },
     size: { // specify the chart size
       height: 340,
       width: 680
